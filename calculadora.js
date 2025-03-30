68b34a45043e4a1cf0f0897b7aa0164da917524f
raamomm <ramommrocha36@gmail.com>
2025-03-30 15:35:06 -0300

    Calculadora de principais operações aritméticas

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+let numA = 20
+let numB = 5
+let soma = numA + numB
+let subtracao = numA - numB
+let mutiplicacao = numA * numB
+let divisao = numA / numB
+
+
+console.log("O resultado da soma de numA e numB é: " + soma)
+console.log("O resultado da subtração de numA e numB é: " + subtracao)
+console.log("O resultado da multiplicação de numA e numB é: " + mutiplicacao)
+console.log("O resultado da divisão de numA e numB é: " + divisao)
+
